75b6da7fb9f8a10c0d78bb240884ebbd835bb784
wallafi01 <wallafisantos98@outlook.com>
2024-09-07 13:52:57 -0300

    Versao 1

--- main.sh
+++ main.sh
@@ -20,23 +20,3 @@ echo "comment-id=${comment_id}" >> $GITHUB_OUTPUT
 
 echo "Comentário adicionado com sucesso! ID do comentário: $comment_id"
 
- 
-
-Arquivo Dockerfile
-
-# Use uma imagem base do Linux. Aqui, usamos o Debian por ser uma escolha popular.
-FROM debian:latest
-
-# Instale as dependências necessárias
-RUN apt-get update && \
-    apt-get install -y curl jq && \
-    rm -rf /var/lib/apt/lists/*
-
-# Copie o script shell para o container
-COPY main.sh /main.sh
-
-# Torne o script executável
-RUN chmod +x /main.sh
-
-# Defina o script como ponto de entrada
-ENTRYPOINT ["/main.sh"]
